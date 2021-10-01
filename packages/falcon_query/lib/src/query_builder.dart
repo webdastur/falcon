21364b97d9e734e94a85c908a9f5e2201c0e2e4b
webdastur <webdasturuz@gmail.com>
2021-10-01 15:14:21 +0500

    falcon_query doc added.

--- packages/falcon_query/lib/src/query_builder.dart
+++ packages/falcon_query/lib/src/query_builder.dart
@@ -1,41 +1,59 @@
+/// [QueryBuilder] is SQL Query Builder class
 class QueryBuilder {
-  late StringBuffer _buffer;
-  static QueryBuilder? _instance;
-
   QueryBuilder._internal() {
     _buffer = StringBuffer();
   }
 
+  late StringBuffer _buffer;
+  static QueryBuilder? _instance;
+
+  /// [instance] return [QueryBuilder] instance
   static QueryBuilder get instance {
-    if (_instance == null) {
-      _instance = QueryBuilder._internal();
-    }
+    _instance ??= QueryBuilder._internal();
     return _instance!;
   }
 
+  /// [i] short form of [instance]
   static QueryBuilder get i => instance;
 
   void _write(String value) {
-    _buffer.write(" $value");
+    _buffer.write(' $value');
   }
 
+  /// Select columns
+  ///
+  /// Select [columns] Ex: `SELECT column1, column2`
   QueryBuilder select(List<String> columns) {
     _write("SELECT ${columns.join(", ")}");
     return this;
   }
 
+  /// Select All Columns
+  ///
+  /// Ex: `SELECT *`
   QueryBuilder selectAll() {
-    return select(["*"]);
+    return select(['*']);
   }
 
+  /// FROM statement
+  ///
+  /// Ex:
+  /// ```dart
+  /// QueryBuilder.i.selectAll().from('tableName').build();
+  /// ```
+  /// Return:
+  /// ```sql
+  /// SELECT * FROM tableName;
+  /// ```
   QueryBuilder from(String tableName) {
-    _write("FROM ${tableName}");
+    _write('FROM $tableName');
     return this;
   }
 
+  /// Returns completed SQL Query
   String build() {
     var result = _buffer.toString().trim();
     _buffer.clear();
-    return "$result;";
+    return '$result;';
   }
 }
--- packages/falcon_query/test/falcon_query_test.dart
+++ packages/falcon_query/test/falcon_query_test.dart
@@ -2,18 +2,18 @@ import 'package:falcon_query/falcon_query.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group("SELECT ", () {
+  group('SELECT ', () {
     test("all from 'tableName'", () {
-      String query = QueryBuilder.i.selectAll().from("tableName").build();
-      expect(query, equals("SELECT * FROM tableName;"));
+      var query = QueryBuilder.i.selectAll().from('tableName').build();
+      expect(query, equals('SELECT * FROM tableName;'));
     });
 
     test("column1 and column2 from 'tableName'", () {
-      String query = QueryBuilder.i
-          .select(["column1", "column2"])
-          .from("tableName")
+      var query = QueryBuilder.i
+          .select(['column1', 'column2'])
+          .from('tableName')
           .build();
-      expect(query, equals("SELECT column1, column2 FROM tableName;"));
+      expect(query, equals('SELECT column1, column2 FROM tableName;'));
     });
   });
 }
